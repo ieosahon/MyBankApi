b58665afdef7875dcf474374f16ad4ef23e5d67b
Osahon Ighodaro <osahonie@gmail.com>
2022-06-19 10:34:41 +0100

    done

--- MyBankApi/Models/Account.cs
+++ MyBankApi/Models/Account.cs
@@ -1,6 +1,33 @@
-﻿namespace MyBankApi.Models
+﻿using System;
+
+namespace MyBankApi.Models
 {
     public class Account
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal AccountBalance { get; set; }
+        public AccountType AccountType { get; set; }
+        public string AccountNumberGenerated { get; set; }
+        
+        // pin hash and salt used to encrypt the account transaction pin
+        public byte[] PinHash { get; set; }
+        public byte[] PinSalt { get; set; }
+        public DateTime  DateCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+
+
+    }
+
+    public enum AccountType
+    {
+        Savings,
+        Current,
+        Corporate,
+        Government
     }
 }
