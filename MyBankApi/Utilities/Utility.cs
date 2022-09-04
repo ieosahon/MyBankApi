41c6c7671c09df04d121918ed8492ef8b1d4f010
Osahon Ighodaro <osahonie@gmail.com>
2022-09-04 03:57:20 +0100

    refactored codes

---
+++ MyBankApi/Utilities/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyBankApi.Utilities
+{
+    public class Utility
+    {
+        public static void CreatePinHash(string pin, out byte[] pinHash, out byte[] pinSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                pinSalt = hmac.Key;
+                pinHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(pin));
+            }
+        }
+    }
+}
