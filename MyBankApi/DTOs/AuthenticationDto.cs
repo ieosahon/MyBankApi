5c9f31cb524c96286cb3f0124add52f19883a19e
Osahon Ighodaro <ieosahon@gmail.com>
2022-09-06 22:54:25 +0100

    implemented some endpoints

---
+++ MyBankApi/DTOs/AuthenticationDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBankApi.DTOs
+{
+    public class AuthenticationDto
+    {
+        private const string V = "@^[2][0-9]/d{9}*$";
+        private const string U = "@^[0-9]/d{4}*$";
+
+        [Required]
+        [RegularExpression(V, ErrorMessage = "Account number must be 10 digits")]
+        public string AccountNumber { get; set; }
+        [Required]
+        [RegularExpression(U, ErrorMessage = "Pin must be 4 digits")]
+        public string Pin { get; set; }
+    }
+}
